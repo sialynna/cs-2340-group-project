af08bded739e480096b612580b6f82bd96c4353b
jzplusplus <jzplusplus@23f568b9-3fcf-5063-88a9-3d800dbc10ba>
2011-10-07 16:41:02 +0000

    Fixed About window ending entire program.

--- trunk/CS2340 Group Project/src/gui/AboutWindow.java
+++ trunk/CS2340 Group Project/src/gui/AboutWindow.java
@@ -40,7 +40,7 @@ public class AboutWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel background = new JLabel("");
