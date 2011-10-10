18105dbcdfeef8f9b68d31125c077727d7ef5d43
tombellitire@gmail.com <tombellitire@gmail.com@23f568b9-3fcf-5063-88a9-3d800dbc10ba>
2011-10-10 22:10:40 +0000

    Commit.

---
+++ trunk/CS2340 Group Project/src/classes/Member.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class Member {
+	private String name;
+	
+	
+	public Member(String name){
+		this.name = name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+
+}
