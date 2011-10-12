cb74ae2671870e785b587afb415cf54133046d2a
jzplusplus <jzplusplus@23f568b9-3fcf-5063-88a9-3d800dbc10ba>
2011-10-12 16:55:48 +0000

    Updated Supplies to be flexible for more items

--- trunk/CS2340 Group Project/src/classes/Supplies.java
+++ trunk/CS2340 Group Project/src/classes/Supplies.java
@@ -1,17 +1,38 @@
 package classes;
 
 public class Supplies {
-	private int rations = 0;
-	private int ammo = 0;
-	private int cloths = 0;
+	public enum Item{
+		RATIONS, AMMO, CLOTHES, WHEELS
+	}
+	
+	private int[] items;
+	private int money = 0;
 	
 	public Supplies(){
+		items = new int[Item.values().length];
+		for(int i=0; i<items.length; i++)
+		{
+			items[i] = 0;
+		}
+	}
+
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public void addMoney(int addition) {
+		money += addition;
 	}
 	
-	public void setRations(int rations){
-		this.rations = rations;
+	public int getItem(Item item){
+		return items[item.ordinal()];
 	}
-	public int getRations(){
-		return rations;
+	public void setItem(Item item, int amt){
+		items[item.ordinal()] = amt;
+	}
+	public void addItem(Item item, int addition){
+		items[item.ordinal()] += addition;
 	}
 }
