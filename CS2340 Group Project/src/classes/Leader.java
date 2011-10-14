d2d7df0bf5dc811f0f85545927cea6a3b6031834
chickdigger802 <chickdigger802@gmail.com>
2011-10-14 03:58:30 +0000

    did some basic javadoc

--- CS2340 Group Project/src/classes/Leader.java
+++ CS2340 Group Project/src/classes/Leader.java
@@ -1,22 +1,47 @@
 package classes;
 
+/**
+ * leader class
+ * 
+ *
+ */
 public class Leader extends Member {
 	private String profession;
 	
+	/**
+	 * constructor for leader
+	 * @param name the name.
+	 * @param profession his job.
+	 */
 	public Leader(String name, String profession){
 		super(name);
 		this.profession = profession;
 	}
-	
+	/**
+	 * getter for profession
+	 * @return his profession
+	 */
 	public String getProfession(){
 		return profession;
 	}
+	/**
+	 * setter for profession
+	 * @param profession the value
+	 */
 	public void setProfession(String profession){
 		this.profession = profession;
 	}
+	/**
+	 * getter for name
+	 * @return the name.
+	 */
 	public String getName(){
 		return super.getName();
 	}
+	/**
+	 * setter for name
+	 * @param name the name. 
+	 */
 	public void setName(String name){
 		super.setName(name);
 	}
--- CS2340 Group Project/src/classes/Pace.java
+++ CS2340 Group Project/src/classes/Pace.java
@@ -1,18 +1,35 @@
 package classes;
 
+/**
+ * Pace class
+ * 
+ *
+ */
 public class Pace 
 {
 	private int pacePerDay;
 
+	/**
+	 * constructor for pace
+	 * @param initialPace the starting value.
+	 */
 	public Pace(int initialPace)
 	{
 		this.pacePerDay = initialPace;
 	}
 	
+	/**
+	 * setter for pace
+	 * @param pacePerDay the amount of pace.
+	 */
 	public void setPace(int pacePerDay)
 	{
 		this.pacePerDay = pacePerDay;
 	}
+	/**
+	 * getter for pace.
+	 * @return the pace.
+	 */
 	public int getPace()
 	{
 		return pacePerDay;
