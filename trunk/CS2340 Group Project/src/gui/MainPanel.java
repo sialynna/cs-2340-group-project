07e608e618ab3e3af46b78e5f2d58a000217a0d7
Christopher.Whittemore <Christopher.Whittemore@23f568b9-3fcf-5063-88a9-3d800dbc10ba>
2011-11-04 17:25:14 +0000

    MainPanel with map uploaded

--- trunk/CS2340 Group Project/src/gui/MainPanel.java
+++ trunk/CS2340 Group Project/src/gui/MainPanel.java
@@ -2,6 +2,8 @@ package gui;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class MainPanel extends JPanel {
 
@@ -11,7 +13,28 @@ public class MainPanel extends JPanel {
 	public MainPanel() {
 		setLayout(null);
 		
+		JButton moveButton = new JButton("");
+		moveButton.setBorderPainted(false);
+		moveButton.setBounds(568, 60, 20, 20);
+		add(moveButton);
+		
+		JButton rationsButton = new JButton("");
+		rationsButton.setBorderPainted(false);
+		rationsButton.setBounds(568, 114, 20, 20);
+		add(rationsButton);
+		
+		JButton paceButton = new JButton("");
+		paceButton.setBorderPainted(false);
+		paceButton.setBounds(568, 142, 20, 20);
+		add(paceButton);
+		
+		JButton menuButton = new JButton("");
+		menuButton.setBorderPainted(false);
+		menuButton.setBounds(568, 170, 20, 20);
+		add(menuButton);
+		
 		JLabel background = new JLabel("background");
+		background.setIcon(new ImageIcon(MainPanel.class.getResource("/gui/resources/Map Display.jpg")));
 		background.setBounds(0, 0, 720, 480);
 		add(background);
 		
