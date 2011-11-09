7e3b7d8afb542612c19b4db758cf335b097f1fee
chickdigger802 <chickdigger802@gmail.com>
2011-11-09 07:50:22 +0000

    added names for locations

--- CS2340 Group Project/src/classes/Map.java
+++ CS2340 Group Project/src/classes/Map.java
@@ -15,31 +15,6 @@ public class Map
 	}
 	
 	
-	/**
-	 * returns the landmark we need.
-	 */
-	public void getLandmark()
-	{
-		if(isLocation(distance))
-		{
-			
-		}
-	}
-	/**
-	 * checks if wagon passed a location yet.
-	 * @param distance current location
-	 * @return true or false
-	 */
-	public boolean isLocation(int distance)
-	{
-		this.distance=distance;
-		if((distance>=100&&distance<=140)||(distance>=250&&distance<=290)||(distance>=420&&distance<=480)||(distance>=560&&distance<=600)||(distance>=750&&distance<=790)||(distance>=900&&distance<=940)||(distance>=1100&&distance<=1140)||(distance>=1250&&distance<=1290)||(distance>=1500))
-		{
-			return true;
-		}
-		else 
-			return false;
-	}
-	
+
 
 }
