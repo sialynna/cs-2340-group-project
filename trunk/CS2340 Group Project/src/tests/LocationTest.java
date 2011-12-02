d27d0dae9d72f4c3ebccb4c7ee5467e6477eb1a6
Christopher.Whittemore <Christopher.Whittemore@23f568b9-3fcf-5063-88a9-3d800dbc10ba>
2011-12-02 17:50:43 +0000

    JUnit test uploaded

---
+++ trunk/CS2340 Group Project/src/tests/LocationTest.java
@@ -0,0 +1,87 @@
+package tests;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import classes.Location;
+import classes.Pace;
+
+/**
+ * This test checks the updateLocation() method in the Location class
+ *
+ */
+
+public class LocationTest extends TestCase {
+	
+	Pace pace;
+	Location loc;
+	@Before
+	protected void setUp()
+	{
+		pace = new Pace();
+		loc = new Location(0, pace);
+	}
+	
+	//Test the easy pace setting
+	@Test
+	public void testPaceEasy()
+	{
+		pace.setPace(1);
+		loc.updateLocation();
+		assertEquals("Easy Pace Fails", 15.0, loc.getLocation());
+		loc.updateLocation();
+		assertEquals("Easy Pace Fails", 30.0, loc.getLocation());
+	}
+	
+	//Test the normal pace setting
+	@Test
+	public void testPaceNormal()
+	{
+		pace.setPace(2);
+		loc.updateLocation();
+		assertEquals("Normal Pace Fails", 25.0, loc.getLocation());
+		loc.updateLocation();
+		assertEquals("Normal Pace Fails", 50.0, loc.getLocation());
+	}
+	
+	//Test the grueling pace setting
+	@Test
+	public void testPaceGrueling()
+	{
+		pace.setPace(3);
+		loc.updateLocation();
+		assertEquals("Grueling Pace Fails", 40.0, loc.getLocation());
+		loc.updateLocation();
+		assertEquals("Grueling Pace Fails", 80.0, loc.getLocation());
+	}
+	
+	//Test changing the pace between moves
+	@Test
+	public void testPaceChange()
+	{
+		pace.setPace(1);
+		loc.updateLocation();
+		assertEquals("Easy Ini Fails", 15.0, loc.getLocation());
+		pace.setPace(2);
+		loc.updateLocation();
+		assertEquals("Normal Change Fails", 40.0, loc.getLocation());
+		pace.setPace(3);
+		loc.updateLocation();
+		assertEquals("Grueling Change Fails", 80.0, loc.getLocation());
+	}
+	
+	//Test setting the landmark correctly
+	@Test
+	public void testSetLandmark()
+	{
+		pace.setPace(3);
+		loc.updateLocation();
+		loc.updateLocation();
+		loc.updateLocation();
+		assertEquals("Location wrong after 3 moves", 120.0, loc.getLocation());
+		assertEquals("Landmark is wrong", 0, loc.getLandmarkAt());
+		loc.updateLocation();
+		assertEquals("Location wrong after arriving at landmark 1", 140.0, loc.getLocation());
+		assertEquals("Landmark is wrong after arriving at landmark 1", 1, loc.getLandmarkAt());
+	}
+}
---
+++ trunk/CS2340 Group Project/src/tests/TransactionTest.java
@@ -0,0 +1,16 @@
+package tests;
+import junit.framework.TestCase;
+import classes.Transaction;
+import classes.Supplies;
+import gui.MainFrame;
+
+public class TransactionTest extends TestCase {
+
+	protected void setUp()
+	{
+		
+		
+		
+	}
+	
+}
